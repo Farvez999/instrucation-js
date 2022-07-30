8f5588c4ae040112adb6478591e89482c450cb9b
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-07-31 00:17:23 +0600

    variable type

---
+++ math.js
@@ -0,0 +1,18 @@
+/** 
+ * 1. number = 55, 9854,-32
+ * 2. integer = 2, 5
+ * 3. float= 2.4, 5.6, 32.69
+ * 4. Operation = addition, substraction, multiplication, division, remainder
+ * 5. parcent,
+ * 6. average
+ * 7. comparision: >, <, ==, !=,<=, >=
+ * 8. oikik: 2 kilo apple and 5kilo mango:
+ * 9. 45, 35, 85, 23,56 =max, min average ,sum ber kora
+ * 10. series
+ * Extra:
+ * 1. Prime
+ * 2.Permutation
+ * 3. Area
+ * Alaways:
+ * Jodi lage tahole aro sikhe nibo.
+ * */
---
+++ variable.js
@@ -0,0 +1,16 @@
+// numeric type
+var price = 11;
+var age = 24;
+var temperature = 38;
+var wight = 45;
+var tk = 120;
+
+// String type
+var name = "sodor ali"
+var location = " andor killa bandorban"
+var special = 'alia bhatt'
+
+// boolen
+var serious = true;
+var fullMarks = false;
+var single = false;
