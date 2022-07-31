fbdafefa81d78ee0791be90c218f0d084e9caabc
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-07-31 10:39:47 +0600

    Varible change

---
+++ shorthand.js
@@ -0,0 +1,28 @@
+// variable declaration
+var tomatoPrice = 25;
+// variable update
+tomatoPrice = 30;
+// Add somethings to the previous value
+var price1 = 31;
+var price2 = 20;
+
+//set a new price
+// price1 = 35;
+
+//add some value to the existing value
+price1 = price1 + 10;
+console.log(price1)
+
+//addition to the same variable shorthand
+//+= means adding some value to the existing value
+price1 += 10;
+
+var age = 24;
+age = age - 1;
+//++ means adding 10to the existing
+age++;
+
+var love = 100;
+love = love - 1;
+//-- means deducting 10to the existing
+love--;
